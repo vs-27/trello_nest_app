aa223fa175f34fc18c74dde88053e3e011f8f1d2
Viktor <victor.shkapoid@gmail.com>
2025-01-16 13:04:25 +0100

    Add reg ajax callback

--- public/login/login.js
+++ public/login/login.js
@@ -1,6 +1,6 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const container = document.getElementById('auth-container');
 
 signUpButton.addEventListener('click', () => {
   container.classList.add("right-panel-active");
@@ -8,4 +8,31 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
+});
+
+const regContainerSelector = '#auth-container.right-panel-active';
+const loginContainerSelector = '#auth-container:not(.right-panel-active)';
+
+$(document).ready(function() {
+  $('body').on('click', `${regContainerSelector} #signup-btn`, (e) => {
+    const regFormData = new FormData();
+    regFormData.append('fullName', $('#reg_full_name').val());
+    regFormData.append('email', $('#reg_email').val());
+    regFormData.append('password', $('#reg_password').val());
+
+    $.ajax({
+      url: '/auth/reg',
+      type: 'POST',
+      contentType: false,
+      processData: false,
+      dataType: 'json',
+      data: regFormData,
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(error) {
+        alert('Login failed. Please check your credentials.');
+      }
+    });
+  });
 });
