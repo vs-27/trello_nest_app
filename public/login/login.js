08c407862878a87c34d7f9ea21422aaa74d2b0a8
Viktor <victor.shkapoid@gmail.com>
2025-01-16 16:03:39 +0100

    Add log ajax callback and fixed reg callback

--- public/login/login.js
+++ public/login/login.js
@@ -10,29 +10,39 @@ signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
 
-const regContainerSelector = '#auth-container.right-panel-active';
-const loginContainerSelector = '#auth-container:not(.right-panel-active)';
-
-$(document).ready(function() {
-  $('body').on('click', `${regContainerSelector} #signup-btn`, (e) => {
-    const regFormData = new FormData();
-    regFormData.append('fullName', $('#reg_full_name').val());
-    regFormData.append('email', $('#reg_email').val());
-    regFormData.append('password', $('#reg_password').val());
-
+$(document).ready(() => {
+  const handleFormSubmit = (url, formData, error) => {
     $.ajax({
-      url: '/auth/reg',
+      url: url,
       type: 'POST',
       contentType: false,
       processData: false,
       dataType: 'json',
-      data: regFormData,
-      success: function(response) {
+      data: formData,
+      success: (response) => {
         console.log(response);
       },
-      error: function(error) {
-        alert('Login failed. Please check your credentials.');
+      error: (err) => {
+        alert(`${error}`);
       }
     });
+  };
+  
+  $('#log-form').on('submit', (e) => {
+    e.preventDefault();
+    handleFormSubmit(
+      '/auth/log',
+      new FormData($('#log-form')[0]),
+      'Login failed. Please check your credentials.'
+    );
+  });
+  
+  $('#reg-form').on('submit', (e) => {
+    e.preventDefault();
+    handleFormSubmit(
+      '/auth/reg',
+      new FormData($('#reg-form')[0]),
+      'Registration failed. Please check your credentials.'
+    );
   });
 });
