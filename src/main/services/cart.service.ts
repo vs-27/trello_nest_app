0c906e265b4ab08519402d4f1b520bd3d3d17c7e
Viktor <victor.shkapoid@gmail.com>
2025-01-23 18:24:08 +0100

    TREL-33/34 Add to cart and delete from cart

--- src/main/services/cart.service.ts
+++ src/main/services/cart.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { RedisStoreService } from '../../services/redis-store.service';
+
+export const REDIS_CART_KEY_PREFIX = 'CART_';
 
 @Injectable()
 export class CartService {
   constructor(
+    private readonly redisStore: RedisStoreService,
   ) {}
 
+  async getCart(userId: number, defaultValue: any = null): Promise<any> {
+    let value = await this.redisStore.get(CartService.generateCartRedisKey(userId));
+    return value ? value : defaultValue;
+  }
+
+  async addToCart(userId: number, productDto) {
+    let cartProducts = await this.getCart(userId, []);
+    cartProducts.push(productDto);
+
+    await this.redisStore.set(CartService.generateCartRedisKey(userId), cartProducts)
+  }
+
+  async removeFromCart(userId: number, productId: number) {
+    const cartProducts = (await this.getCart(userId)).filter(({ id }) => id !== productId);
+    await this.redisStore.set(CartService.generateCartRedisKey(userId), cartProducts)
+  }
+
+  static generateCartRedisKey(userId: number) {
+    return `${REDIS_CART_KEY_PREFIX}_${userId}`;
+  }
 }
