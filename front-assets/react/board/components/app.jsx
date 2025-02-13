35d8e83c964f552a0b3eea11f177c27baee5255d
Viktor <victor.shkapoid@gmail.com>
2025-02-13 19:27:56 +0100

    Board. Migrate component from class to function style

--- front-assets/react/board/components/app.jsx
+++ front-assets/react/board/components/app.jsx
@@ -1,109 +1,119 @@
 import './app.less'
-
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Board from 'react-trello'
 
-const data = require('./data.json');
+const data = require('./data.json')
 
+// Handle drag start and end
 const handleDragStart = (cardId, laneId) => {
-  console.log('drag started');
-  console.log(`cardId: ${cardId}`);
+  console.log('drag started')
+  console.log(`cardId: ${cardId}`)
   console.log(`laneId: ${laneId}`)
-};
+}
 
 const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
-  console.log('drag ended');
-  console.log(`cardId: ${cardId}`);
-  console.log(`sourceLaneId: ${sourceLaneId}`);
+  console.log('drag ended')
+  console.log(`cardId: ${cardId}`)
+  console.log(`sourceLaneId: ${sourceLaneId}`)
   console.log(`targetLaneId: ${targetLaneId}`)
-};
+}
 
-class App extends Component {
-  state = { boardData: { lanes: [] } };
+// Functional component for App
+const App = () => {
+  // State hook for board data and event bus
+  const [boardData, setBoardData] = useState({ lanes: [] })
+  const [eventBus, setEventBus] = useState(null)
   
-  setEventBus = (eventBus) => {
-    this.setState({ eventBus })
-  };
+  // Fetch the board data on mount
+  useEffect(() => {
+    const fetchBoardData = async () => {
+      const response = await getBoard()
+      setBoardData(response)
+    }
+    fetchBoardData()
+  }, [])
   
-  async componentDidMount() {
-    const response = await this.getBoard();
-    this.setState({ boardData: response })
-  }
-  
-  getBoard() {
+  // Get board data (mocked with a resolved promise)
+  const getBoard = () => {
     return new Promise((resolve) => {
       resolve(data)
     })
   }
   
-  completeCard = () => {
-    this.state.eventBus.publish({
-      type: 'ADD_CARD',
-      laneId: 'COMPLETED',
-      card: {
-        id: 'Milk',
-        title: 'Buy Milk',
-        label: '15 mins',
-        description: 'Use Headspace app',
-      },
-    });
-    this.state.eventBus.publish({
-      type: 'REMOVE_CARD',
-      laneId: 'PLANNED',
-      cardId: 'Milk',
-    })
-  };
-  
-  addCard = () => {
-    this.state.eventBus.publish({
-      type: 'ADD_CARD',
-      laneId: 'BLOCKED',
-      card: {
-        id: 'Ec2Error',
-        title: 'EC2 Instance Down',
-        label: '30 mins',
-        description: 'Main EC2 instance down',
-      },
-    })
-  };
-  
-  shouldReceiveNewData = (nextData) => {
-    console.log('New card has been added');
-    console.log(nextData)
-  };
-  
-  handleCardAdd = (card, laneId) => {
-    console.log(`New card added to lane ${laneId}`);
-    console.dir(card)
-  };
-  
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h3>React Trello Demo</h3>
-        </div>
-        <div className="App-intro">
-          <button onClick={this.completeCard} style={{ margin: 5 }}>
-            Complete Buy Milk
-          </button>
-          <button onClick={this.addCard} style={{ margin: 5 }}>
-            Add Blocked
-          </button>
-          <Board
-            editable
-            onCardAdd={this.handleCardAdd}
-            data={this.state.boardData}
-            draggable
-            onDataChange={this.shouldReceiveNewData}
-            eventBusHandle={this.setEventBus}
-            handleDragStart={handleDragStart}
-            handleDragEnd={handleDragEnd}
-          />
-        </div>
-      </div>
-    )
+  // Complete the 'Buy Milk' card
+  const completeCard = () => {
+    if (eventBus) {
+      eventBus.publish({
+        type: 'ADD_CARD',
+        laneId: 'COMPLETED',
+        card: {
+          id: 'Milk',
+          title: 'Buy Milk',
+          label: '15 mins',
+          description: 'Use Headspace app',
+        },
+      })
+      eventBus.publish({
+        type: 'REMOVE_CARD',
+        laneId: 'PLANNED',
+        cardId: 'Milk',
+      })
+    }
   }
+  
+  // Add a new card to the blocked lane
+  const addCard = () => {
+    if (eventBus) {
+      eventBus.publish({
+        type: 'ADD_CARD',
+        laneId: 'BLOCKED',
+        card: {
+          id: 'Ec2Error',
+          title: 'EC2 Instance Down',
+          label: '30 mins',
+          description: 'Main EC2 instance down',
+        },
+      })
+    }
+  }
+  
+  // Handle new data received
+  const shouldReceiveNewData = (nextData) => {
+    console.log('New card has been added')
+    console.log(nextData)
+  }
+  
+  // Handle card addition to a lane
+  const handleCardAdd = (card, laneId) => {
+    console.log(`New card added to lane ${laneId}`)
+    console.dir(card)
+  }
+  
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h3>React Trello Demo</h3>
+      </div>
+      <div className="App-intro">
+        <button onClick={completeCard} style={{ margin: 5 }}>
+          Complete Buy Milk
+        </button>
+        <button onClick={addCard} style={{ margin: 5 }}>
+          Add Blocked
+        </button>
+        <Board
+          editable
+          onCardAdd={handleCardAdd}
+          data={boardData}
+          draggable
+          onDataChange={shouldReceiveNewData}
+          eventBusHandle={setEventBus}
+          handleDragStart={handleDragStart}
+          handleDragEnd={handleDragEnd}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App
